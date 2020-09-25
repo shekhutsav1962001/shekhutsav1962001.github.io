4df301ba343aa718b56d59d658f8e572e33539c6
shekhutsav162001 <shekhutsav1962001@gmail.com>
2020-09-25 12:49:17 +0530

    cart animation
    cart animation

--- LAB3/js/cart.js
+++ LAB3/js/cart.js
@@ -1,3 +1,12 @@
+// $('.btnn').click(function(){
+//   var a = $(this)
+//   console.log(this);
+//   console.log(a.parent().parent().parent());
+//   var b =a.parent().parent().parent();
+//     b.fadeOut(1000,function(){
+//         $(this).remove()
+// })
+// })
 
 function loading() {
   var ttl = 0;
@@ -51,22 +60,22 @@ function qtychange(event) {
 }
 
 function removeCartItems(event) {
-  var buttonClicked = event.target
-  var parent = buttonClicked.parentElement.parentElement.parentElement;
-  parent.style.transition = "all 400ms";
-  parent.style.opacity = '0';
-  setTimeout(function(){
-  parent.remove();
-  if ($('button.btnn').length <=0)
-  {
-    $("#container1").remove();
-    $("#container2").html(`<h1>Empty Cart</h1><br><img src="pic/empty-cart.png" class="img-fluid">`);
-  }
-  updateCartTotal();
-  },400);
+  $('.btnn').click(function () {
+    var a = $(this)
+    var b = a.parent().parent().parent();
+    b.fadeOut(700, function () {
+      b.remove();
+      updateCartTotal();
+      if ($('button.btnn').length <= 0) {
+        $("#container1").remove();
+        $("#container2").html(`<h1>Empty Cart</h1><br><img src="pic/empty-cart.png" class="img-fluid">`);
+      }
+    })
+  })
 }
 
 function updateCartTotal() {
+  
   var ttl = 0;
   var Ip = $("input");
   var prc = $(".product-price")
@@ -74,7 +83,35 @@ function updateCartTotal() {
   for (var i = 0; i < prc.length; i++) {
     ttl += parseInt(Ip[i].value) * parseInt(prc[i].innerHTML)
   }
-  document.getElementsByClassName("tot")[0].innerHTML = ttl;
+  $(".tot").fadeOut(500,function()
+  {
+    var abc = $(this)
+    abc.text(ttl);
+  })
+  $(".tot").fadeIn(300,function()
+  {
+    var abc = $(this)
+    abc.text(ttl);
+  })
+
+  $(".gt").fadeOut(500,function()
+  {
+    var abc = $(this)
+    abc.text(ttl+50);
+  })
+  $(".gt").fadeIn(300,function()
+  {
+    var abc = $(this)
+    abc.text(ttl+50);
+  })
+
+  $(".dc").fadeOut(500,function()
+  {
+  })
+  $(".dc").fadeIn(300,function()
+  {
+    
+  })
   var gt = document.getElementsByClassName('gt')[0]
   gt.innerHTML = ttl + 50
 }
